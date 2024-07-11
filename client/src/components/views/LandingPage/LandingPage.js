116577b8a8bc4f91b8233acd86360cd818fd894a
hansangmin <test@test.com>
2024-07-11 17:36:24 +0900

    cors 정책 해결

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-// import { response } from "express";  response 변수로 받는 설정에서 자동으로 import되어 오류 발생.... 
+// import { response } from "express";  response 변수로 받는 설정에서 자동으로 import되어 오류 발생....
 
 function LandingPage() {
   useEffect(() => {
-    axios.get("/api/hello").then((response) => console.log(response.data));
+    axios
+      .get("http://localhost:4000/api/hello")
+      .then((response) => console.log(response.data));
   }, []);
 
   return <div>LandingPage </div>;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 4000; //5000번 포트는 airplay에서 사용....
 
 const mongoose = require("mongoose");
 const config = require("./config/key"); //key 현재 환경을 읽어와서 mongoDB 접속 주소 어떻게 줄지 정함
 
+const cors = require("cors");
+
 const { Post } = require("./config/models/Post");
+
+//CORS 정책
+// proxy
+
+// cors : express에서 cors 설정 -> 화면단에서 경로 localhost:XXXX 으로 잡아줘야함
+app.use(cors());
 
 //바디파서 : 클라이언트에서 오는 정보를 서버에서 분석해서 들여올수있게 하는것
 //버전 업으로 express 에서 사용 가능.
@@ -18,7 +25,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/api/hello", (req, res) => {
-  res.send("Hello World!!!!!!!");
+  res.send("Hello");
 });
 
 app.post("/regi", (req, res) => {
@@ -28,6 +35,8 @@ app.post("/regi", (req, res) => {
     .then((userInfo) => res.status(200).json({ success: true })) //status(200) 통신 성공 이라는 뜻...!
     .catch((err) => res.json({ success: false, err }));
 });
+
+const port = 4000; //5000번 포트는 airplay에서 사용....
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
